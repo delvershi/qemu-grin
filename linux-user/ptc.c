35ffaf6ce29e8249c3f177c8588a358fb4f3b236
sonnyT <tianlinan@iie.ac.cn>
2020-10-26 00:30:01 +0800

    mask syscall

--- linux-user/ptc.c
+++ linux-user/ptc.c
@@ -1044,6 +1044,19 @@ unsigned long ptc_do_syscall2(void){
       fprintf(stderr,"Mask kill syscall\n");
       return env->eip;//TARGET_NR_futex
     }
+    if(env->regs[R_EAX]==59){
+      env->eip = env->exception_next_eip;
+      cpu->exception_index = -1;
+      fprintf(stderr,"Mask execve syscall\n");
+      return env->eip;//TARGET_NR_futex
+    }
+    if(env->regs[R_EAX]==2 ||
+       env->regs[R_EAX]==3){
+      env->eip = env->exception_next_eip;
+      cpu->exception_index = -1;
+      fprintf(stderr,"Mask open close syscall\n");
+      return env->eip;//TARGET_NR_futex
+    }
     if(env->regs[R_EAX]==202){
       env->eip = env->exception_next_eip;
       cpu->exception_index = -1;
