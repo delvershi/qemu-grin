dcc80c00e5be6546b46c5c85b0bd50de45b6796d
sonnylier <sonny_lier@163.com>
2020-12-08 08:56:22 +0800

    modifing the range of image address

--- linux-user/ptc.c
+++ linux-user/ptc.c
@@ -1012,7 +1012,7 @@ uint32_t ptc_is_image_addr(uint64_t va){
     return 1;
   }
  
-  if(va<=info->start_stack && va>=0x4000000000)
+  if(va<=info->start_stack && va>0x4000000000)
     return 1;
 
   fprintf(stderr,"Unknow address access: %lx\n",va);
