df32ed64d0df81025f072d0fc336c24b78ebf4b7
sonnylier <sonny_lier@163.com>
2020-12-14 21:28:55 +0800

    mask open syscall2

--- linux-user/ptc.c
+++ linux-user/ptc.c
@@ -1050,11 +1050,11 @@ unsigned long ptc_do_syscall2(void){
     if(env->regs[R_EAX]==5 ||
        env->regs[R_EAX]==6 ||
        env->regs[R_EAX]==0 ||
-       env->regs[R_EAX]==4){ 
-       //env->regs[R_EAX]==3){
+       env->regs[R_EAX]==4 || 
+       env->regs[R_EAX]==2 ){
       env->eip = env->exception_next_eip;
       cpu->exception_index = -1;
-      fprintf(stderr,"Mask open lstat fstat read syscall\n");
+      fprintf(stderr,"Mask open lstat fstat read open syscall\n");
       return env->eip;//TARGET_NR_futex
     }
 
